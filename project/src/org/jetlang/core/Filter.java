78530fdd706410462095df207640eedfaea3c285
peter.royal@pobox.com <peter.royal@pobox.com@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2008-08-11 05:12:54 +0000

    javadocs

--- project/src/org/jetlang/core/Filter.java
+++ project/src/org/jetlang/core/Filter.java
@@ -1,10 +1,16 @@
 package org.jetlang.core;
 
 /**
- * User: mrettig
- * Date: Jul 27, 2008
- * Time: 2:32:31 PM
+ * Interface to represent a boolean filter for messages
+ *
+ * @param <T> Type of message this filter is for
  */
 public interface Filter<T> {
+    /**
+     * Check to see whether the supplied message passes the filter
+     *
+     * @param msg Message to check against filter
+     * @return True upon success, false otherwise.
+     */
     boolean passes(T msg);
 }
