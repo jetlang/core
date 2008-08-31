f9ddbb913b5fd886f5b0bb1dd2334e62972518a2
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2008-08-31 16:21:30 +0000

    Add docs.

--- project/src/org/jetlang/fibers/Fiber.java
+++ project/src/org/jetlang/fibers/Fiber.java
@@ -4,6 +4,15 @@ import org.jetlang.core.DisposingExecutor;
 import org.jetlang.core.Scheduler;
 
 
+/**
+ * Fibers provide event queueing, scheduling, and full pub/sub capabilities when combined with a
+ * {@see org.jetlang.channels.Channel}. Fibers can be backed by a dedicated thread ({@see ThreadFiber}) or use
+ * a thread pool ({@see PoolFiberFactory}).
+ * <p/>
+ * Events executed by Fiber will be process sequentially.
+ *
+ * @author mrettig
+ */
 public interface Fiber extends DisposingExecutor, Scheduler {
     /**
      * Start consuming events
