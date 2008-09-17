c2b3d74508dfe388425972db21acbe189f2abfdc
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2008-09-17 21:29:20 +0000

    Use a Lock since it appears to be more efficient.

--- src/main/java/org/jetlang/core/RunnableBlockingQueue.java
+++ src/main/java/org/jetlang/core/RunnableBlockingQueue.java
@@ -2,6 +2,9 @@ package org.jetlang.core;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 
 /**
  * Blocking queue supporting efficient put and sweep operations.
@@ -11,6 +14,8 @@ import java.util.List;
 public class RunnableBlockingQueue {
 
     private volatile boolean _running = true;
+    private Lock _lock = new ReentrantLock();
+    private Condition _waiter = _lock.newCondition();
     private final List<Runnable> _queue = new ArrayList<Runnable>();
 
     public boolean isRunning() {
@@ -22,17 +27,21 @@ public class RunnableBlockingQueue {
     }
 
     public void put(Runnable r) {
-        synchronized (_queue) {
+        _lock.lock();
+        try{
             _queue.add(r);
-            _queue.notify();
+            _waiter.signal();
+        }finally{
+            _lock.unlock();
         }
     }
 
     public Runnable[] sweep() {
-        synchronized (_queue) {
+        _lock.lock();
+        try {
             while (_queue.isEmpty() && _running) {
                 try {
-                    _queue.wait();
+                    _waiter.await();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -40,12 +49,17 @@ public class RunnableBlockingQueue {
             Runnable[] result = _queue.toArray(new Runnable[_queue.size()]);
             _queue.clear();
             return result;
+        }finally{
+            _lock.unlock();
         }
     }
 
     public boolean isEmpty() {
-        synchronized (_queue) {
+        _lock.lock();
+        try{
             return _queue.isEmpty();
+        }finally{
+            _lock.unlock();
         }
     }
 }
