a10316eff02d674383ab09b12d9179cd49fa933a
peter.royal@pobox.com <peter.royal@pobox.com@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2009-02-11 18:35:47 +0000

    fix compile

--- src/test/java/org/jetlang/examples/pingpong/PingPongTest.java
+++ src/test/java/org/jetlang/examples/pingpong/PingPongTest.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 public class PingPongTest {
 
     @Test
-    public void executWithDedicatedThreads() {
+    public void executWithDedicatedThreads() throws Exception {
         PingPongChannels channels = new PingPongChannels();
 
         ThreadFiber pingThread = new ThreadFiber();
