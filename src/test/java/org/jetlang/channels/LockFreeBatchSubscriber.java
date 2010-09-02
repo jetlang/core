7449fc751ba06c298cd16ff87890d1aa589fd0f3
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2010-09-02 01:34:13 +0000

    add example lock free batch sub.

---
+++ src/test/java/org/jetlang/channels/LockFreeBatchSubscriber.java
@@ -0,0 +1,88 @@
+package org.jetlang.channels;
+
+import org.jetlang.core.Callback;
+import org.jetlang.core.Filter;
+import org.jetlang.fibers.Fiber;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author ruslan
+ *         created 22.08.2010 at 12:20:32
+ */
+public class LockFreeBatchSubscriber<T> extends BaseSubscription<T> {
+
+    private final Fiber executor;
+
+    private final Callback<ConcurrentLinkedQueue<T>> callback;
+
+    private final int interval;
+    private final TimeUnit unit;
+
+    /**
+     * number of currently pending messages
+     */
+    private final AtomicBoolean pending = new AtomicBoolean(false);
+
+    /**
+     * we need lock-free Queue implementation here
+     */
+    private final ConcurrentLinkedQueue<T> queue = new ConcurrentLinkedQueue<T>();
+    private final Runnable flushRunnable;
+
+
+    public LockFreeBatchSubscriber(final Fiber executor,
+                                   final Callback<ConcurrentLinkedQueue<T>> receive,
+                                   final Filter<T> filter,
+                                   final int interval,
+                                   final TimeUnit timeUnit) {
+        super(executor, filter);
+
+        this.executor = executor;
+        this.callback = receive;
+        this.interval = interval;
+        this.unit = timeUnit;
+        this.flushRunnable = new Runnable() {
+            public void run() {
+                flush();
+            }
+
+            @Override
+            public String toString() {
+                return "Flushing" + LockFreeBatchSubscriber.this + " via " + callback.toString();
+            }
+        };
+    }
+
+    public LockFreeBatchSubscriber(final Fiber queue,
+                                   final Callback<ConcurrentLinkedQueue<T>> receive,
+                                   final int interval,
+                                   final TimeUnit timeUnit) {
+        this(queue, receive, null, interval, timeUnit);
+    }
+
+    protected void onMessageOnProducerThread(final T msg) {
+        queue.add(msg);
+
+        if (pending.compareAndSet(false, true)) {
+            schedule();
+        }
+    }
+
+    private void flush() {
+        callback.onMessage(queue);
+        pending.compareAndSet(true, false);
+        if (!queue.isEmpty()) {
+            if (pending.compareAndSet(false, true)) {
+                schedule();
+            }
+        }
+    }
+
+    private void schedule() {
+        executor.schedule(flushRunnable, interval, unit);
+    }
+
+}
---
+++ src/test/java/org/jetlang/channels/LockFreeBatchSubscriberPerf.java
@@ -0,0 +1,59 @@
+package org.jetlang.channels;
+
+import org.jetlang.core.Callback;
+import org.jetlang.fibers.ThreadFiber;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class LockFreeBatchSubscriberPerf {
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadFiber fiber = new ThreadFiber();
+        fiber.start();
+        final int total = 20000000;
+        final CountDownLatch latch = new CountDownLatch(1);
+        Callback<ConcurrentLinkedQueue<String>> cb = new Callback<ConcurrentLinkedQueue<String>>() {
+            int count = 0;
+
+            public void onMessage(ConcurrentLinkedQueue<String> message) {
+                for (String val = message.poll(); val != null; val = message.poll()) {
+                    count++;
+                }
+                if (count >= total) {
+                    latch.countDown();
+                }
+            }
+        };
+
+        Callback<List<String>> listCb = new Callback<List<String>>() {
+            int count = 0;
+
+            public void onMessage(List<String> message) {
+                for (int val = 0; val < message.size(); val++) {
+                    String m = message.get(val);
+                    count++;
+                }
+                if (count >= total) {
+                    latch.countDown();
+                    System.out.println("count = " + count);
+                }
+            }
+        };
+        Channel<String> c = new MemoryChannel<String>();
+        //LockFreeBatchSubscriber<String> sub = new LockFreeBatchSubscriber<String>(fiber, cb, 0, TimeUnit.MICROSECONDS);
+        BatchSubscriber<String> sub = new BatchSubscriber<String>(fiber, listCb, 0, TimeUnit.MICROSECONDS);
+
+        c.subscribe(sub);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < total; i++) {
+            c.publish("hello");
+        }
+        boolean b = latch.await(10, TimeUnit.SECONDS);
+        long diff = System.currentTimeMillis() - start;
+        System.out.println("Time: = " + diff + " " + b);
+    }
+}
