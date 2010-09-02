fbbc7f202a87d4a36e76722989944ff2c44aec1a
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2010-09-02 02:54:24 +0000

    q latency test

--- src/test/java/org/jetlang/channels/LockFreeBatchSubscriberPerf.java
+++ src/test/java/org/jetlang/channels/LockFreeBatchSubscriberPerf.java
@@ -1,6 +1,7 @@
 package org.jetlang.channels;
 
 import org.jetlang.core.Callback;
+import org.jetlang.core.MessageReader;
 import org.jetlang.fibers.ThreadFiber;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public class LockFreeBatchSubscriberPerf {
     public static void main(String[] args) throws InterruptedException {
         ThreadFiber fiber = new ThreadFiber();
         fiber.start();
-        final int total = 20000000;
+        final int total = 50000000;
         final CountDownLatch latch = new CountDownLatch(1);
         Callback<ConcurrentLinkedQueue<String>> cb = new Callback<ConcurrentLinkedQueue<String>>() {
             int count = 0;
@@ -42,9 +43,25 @@ public class LockFreeBatchSubscriberPerf {
                 }
             }
         };
+
+        Callback<MessageReader<String>> recyclingCb = new Callback<MessageReader<String>>() {
+            int count = 0;
+
+            public void onMessage(MessageReader<String> message) {
+                for (int val = 0; val < message.size(); val++) {
+                    String m = message.get(val);
+                    count++;
+                }
+                if (count >= total) {
+                    latch.countDown();
+                    System.out.println("count = " + count);
+                }
+            }
+        };
         Channel<String> c = new MemoryChannel<String>();
-        //LockFreeBatchSubscriber<String> sub = new LockFreeBatchSubscriber<String>(fiber, cb, 0, TimeUnit.MICROSECONDS);
-        BatchSubscriber<String> sub = new BatchSubscriber<String>(fiber, listCb, 0, TimeUnit.MICROSECONDS);
+        LockFreeBatchSubscriber<String> sub = new LockFreeBatchSubscriber<String>(fiber, cb, 0, TimeUnit.MICROSECONDS);
+        //BatchSubscriber<String> sub = new BatchSubscriber<String>(fiber, listCb, 0, TimeUnit.MICROSECONDS);
+        //RecyclingBatchSubscriber<String> sub = new RecyclingBatchSubscriber<String>(fiber, recyclingCb, 0, TimeUnit.MICROSECONDS);
 
         c.subscribe(sub);
 
@@ -54,6 +71,6 @@ public class LockFreeBatchSubscriberPerf {
         }
         boolean b = latch.await(10, TimeUnit.SECONDS);
         long diff = System.currentTimeMillis() - start;
-        System.out.println("Time: = " + diff + " " + b);
+        System.out.println(sub.getClass().getSimpleName() + " Time: = " + diff + " " + b);
     }
 }
